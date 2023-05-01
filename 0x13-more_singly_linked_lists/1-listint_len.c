c2888536a2f32f5d12466139f8468156ee7bacf7
MalakShaaban <malakshaaban613@gmail.com>
2023-05-01 03:30:57 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,15 @@
 #include "lists.h"
+
 /**
  * listint_len - returns the number of elements in a linked list
  * @h: pointer to the head of the list
  *
  * Return: number of nodes in the list
  */
-size_t listint_len (const struct node *h)
+size_t listint_len(const struct node *h)
 {
-size_t count =0;
-for (;h!= NULL ; h=h->next)
-	count ++ ;
-
-return count ;
+size_t counter = 0;
+    for (; h != NULL; h = h->next)
+	    counter++;
+return (counter);
 }
