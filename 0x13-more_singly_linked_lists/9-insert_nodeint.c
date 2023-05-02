8d59aa8b84f1ca1a6b883a438703149d71782b0d
MalakShaaban <malakshaaban613@gmail.com>
2023-05-02 11:10:43 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,35 +10,30 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new = NULL;
+listint_t *new_node = NULL;
 listint_t *current;
 	unsigned int i = 0;
 
-	if (!new)
-	return (NULL);
-	new->n = n;
-	new->next = NULL;
-	if (idx == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
-	current = *head;
-	while (current && i < idx - 1)
-	{
-		current = current->next;
-		i++;
-	}
+if (!head)
+return (NULL);
 
-	if (!current)
-	{
-		free(new);
-		return (NULL);
-	}
+if (idx == 0)
+return (add_nodeint(head, n));
+current = *head;
+while (current && i < idx - 1)
+{
+current = current->next;
+i++;
+}
 
-	new->next = current->next;
-	current->next = new;
+if (!current)
+return (NULL);
+new_node = malloc(sizeof(listint_t));
+if (!new_node)
+return (NULL);
+new_node->n = n;
+new_node->next = current->next;
+current->next = new_node;
 
-	return (new);
+return (new_node);
 }
