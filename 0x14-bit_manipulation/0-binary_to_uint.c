d13e79e6a0f41735e1ed0c5f3403aa25a68f53c1
MalakShaaban <malakshaaban613@gmail.com>
2023-05-04 05:25:46 -0700

    bit_manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,17 +12,13 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 
 	if (!b)
-	{
 		return (0);
-	}
 
-	for (; b[i]; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-		{
 			return (0);
-			r = 2 * r + (b[i] - '0');
-		}
+		r = 2 * r + (b[i] - '0');
 	}
 
 	return (r);
