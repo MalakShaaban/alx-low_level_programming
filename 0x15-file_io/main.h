b7ce0c1086a5374b777c1896aca0e9546e006541
MalakShaaban <malakshaaban613@gmail.com>
2023-05-08 12:52:02 -0700

    file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,6 +8,5 @@
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
-void error_file(int filefrom, int fileto, char *argv[]);
 
 #endif
